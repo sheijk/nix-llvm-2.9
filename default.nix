395b48df1f8885ba4b5b6f93036004a4d8e9bd04
Jan Rehders <nospam@sheijk.net>
2021-05-05 21:04:47 +0200

    Created package for LLVM 2.9
    Doesn't build with GCC 4.8, need to find a way to get older version.

---
+++ default.nix
@@ -0,0 +1,28 @@
+{
+  nixpkgs ? (builtins.fetchGit {
+    name = "nixos-stable-20.09";
+    url = "https://github.com/nixos/nixpkgs";
+    ref = "refs/tags/20.09";
+  })
+}:
+
+with import nixpkgs {};
+stdenv.mkDerivation {
+  name = "llvm-2.9";
+
+  src = builtins.fetchTarball {
+    name = "llvm-2.9";
+    url = https://releases.llvm.org/2.9/llvm-2.9.tgz;
+    sha256 = "1wrsbcnbpzcmids2pn19z6nw7384y3k0ivp9qdx0mlb312hn0gcm";
+  };
+
+  buildInputs = [
+    perl
+    # Would be better to use gcc 4.5, see https://gcc.gnu.org/releases.html
+    gcc48
+  ];
+
+  shellHook = ''
+    echo "LLVM 2.9 - a blast from the past!"
+  '';
+}
